a6def63e53ffc39d617bfbccb6fa31942fa5f8ba
Francisco Fuentes <franciscoxfuentes@gmail.com>
2025-12-06 17:15:43 -0600

    upload

--- hw4.js
+++ hw4.js
@@ -512,7 +512,9 @@ slider.addEventListener("input", function() {
     display.innerHTML = newValue;
 });
 
-/* Fetch API to input city and state based on zip code
+// Fetch API function to take state abbreviation and grab the full state name from json file
+
+// (Extra Credit) Fetch API to input city and state based on zip code
     document.getElementById("zip").addEventListener("blur", getZipcode);
         async function getZipcode() {
             try{
@@ -536,7 +538,7 @@ slider.addEventListener("input", function() {
             catch (error) {
                 console.error("Error fetching zip code data: ", error);
             }
-        } */
+        }
 
 // Function for DOM manipulation to show and hide sections
     function showSection(sectionId) {
@@ -694,28 +696,3 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
 
     }
-/* Fetch API to input city and state based on zip code
-    document.getElementById("zip").addEventListener("blur", getZipcode);
-        async function getZipcode() {
-            try{
-                const zip = document.getElementById("zip").value;
-                // API Fetch call with zip value
-                const response = await fetch("https://us-zipcode.api.smarty.com/lookup?key=254252967495540176&city=&state=&zipcode=" + zip);
-                // Error handling
-                if (!response.ok) {
-                    throw new Error("Could not fetch zip code data.");
-                }
-                // Store API data
-                const data = await response.json();
-                console.log(data);
-                // Fill city and state fields
-                const city = data[0].city_states?.[0]?.city;
-                const stateAbbreviation = data[0].city_states?.[0]?.state_abbreviation;
-
-                document.getElementById("city").value = city;
-                document.getElementById("stateAbbreviation").value = stateAbbreviation;
-            }
-            catch (error) {
-                console.error("Error fetching zip code data: ", error);
-            }
-        } */
